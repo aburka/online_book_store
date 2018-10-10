252c2a1848168613ba421ffbc4010db3ee113544
Amy Burka <aburka@gmail.com>
2018-10-10 10:28:36 -0400

    add user, book and author seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create! email: "test@bookstore.dev", password: "testing1"
+
+author = Author.create!(name: "Ron Rash")
+
+titles = ["Serena", "One Foot in Eden", "Saints at the River", "Burning Bright"]
+titles.each do |title|
+  Book.create!(title: title)
+end
+
+author.books << Book.all
